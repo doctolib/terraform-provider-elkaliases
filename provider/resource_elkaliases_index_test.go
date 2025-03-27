9f0df9eae5f479f81460e49731bef657d3087db3
Antoine Verin <antoine.verin@doctolib.com>
2025-03-27 11:18:29 +0100

    fix env var in tests

--- provider/resource_elkaliases_index_test.go
+++ provider/resource_elkaliases_index_test.go
@@ -253,11 +253,11 @@ func TestAccElkaliasesIndex_invalid(t *testing.T) {
 }
 
 func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("ELASTICSEARCH_URL"); v == "" {
-		t.Fatal("ELASTICSEARCH_URL must be set for acceptance tests")
+	if v := os.Getenv("ELKALIASES_URL"); v == "" {
+		t.Fatal("ELKALIASES_URL must be set for acceptance tests")
 	}
-	if v := os.Getenv("ELASTICSEARCH_TOKEN"); v == "" {
-		t.Fatal("ELASTICSEARCH_TOKEN must be set for acceptance tests")
+	if v := os.Getenv("ELKALIASES_TOKEN"); v == "" {
+		t.Fatal("ELKALIASES_TOKEN must be set for acceptance tests")
 	}
 }
 
